31ea3bff144e5b27c0effe0f14702689f4a5dceb
Clément Plantec <plantec.clement@gmail.com>
2020-08-21 21:37:36 +0200

    Update MainViewModel
    MainViewModel can use settings allowing to store screen rects

---
+++ WindowsManager/Helpers/Screen.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Forms = System.Windows.Forms;
+using Drawing = System.Drawing;
+using System.Diagnostics;
+using WindowsManager.Views;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace WindowsManager.Helpers
+{
+    public class Screen
+    {
+
+        #region Fields
+
+        private Forms.Screen _Screen;
+
+        private readonly Orientation _Orientation;
+
+        private SplitWindow _SplitWindow = null;
+
+        #endregion Fields
+
+
+        #region Properties
+
+        public Rect WorkingArea { get; private set; }
+
+        public int Index { get; private set; }
+
+        public Rect[] Rects { get; private set; } = new Rect[3];
+
+        #endregion Properties
+
+
+        #region Events
+
+        public event EventHandler SplitWindowHide;
+
+        #endregion Events
+
+
+        #region Constructor
+
+        public Screen(Forms.Screen screen, int index, bool settingsExist)
+        {
+            _Screen = screen;
+            Index = index;
+            WorkingArea = new Rect(_Screen.WorkingArea.X, _Screen.WorkingArea.Y, _Screen.WorkingArea.Width, _Screen.WorkingArea.Height);
+            _Orientation = WorkingArea.Width >= WorkingArea.Height ? Orientation.Horizontal : Orientation.Vertical;
+
+            if (!settingsExist)
+            {
+                switch (_Orientation)
+                {
+                    case Orientation.Horizontal:
+                        double width = WorkingArea.Width / 3;
+                        double widthFloatingPart = width - Math.Truncate(width);
+
+                        double[] widths;
+                        if (widthFloatingPart < 0.5)
+                            widths = new[] { Math.Floor(width), Math.Ceiling(width), Math.Floor(width), };
+                        else
+                            widths = new[] { Math.Ceiling(width), Math.Ceiling(width), Math.Floor(width), };
+
+                        for (int i = 0; i < 3; i++)
+                            Rects[i] = new Rect(0, 0, widths[i], WorkingArea.Height);
+
+                        break;
+
+                    case Orientation.Vertical:
+                        double height = WorkingArea.Height / 3;
+                        double heightFloatingPart = height - Math.Truncate(height);
+
+                        double[] heights;
+                        if (heightFloatingPart < 0.5)
+                            heights = new[] { Math.Floor(height), Math.Ceiling(height), Math.Floor(height), };
+                        else
+                            heights = new[] { Math.Ceiling(height), Math.Ceiling(height), Math.Floor(height), };
+
+                        for (int i = 0; i < 3; i++)
+                            Rects[i] = new Rect(0, 0, WorkingArea.Width, heights[i]);
+
+                        break;
+                }
+            }
+        }
+
+        #endregion Constructor
+
+
+        #region Public Methods
+
+        public void ShowSplitters()
+        {
+            if (_SplitWindow is null)
+            {
+                _SplitWindow = new SplitWindow
+                {
+                    Left = WorkingArea.X,
+                    Top = WorkingArea.Y,
+                    Width = WorkingArea.Width,
+                    Height = WorkingArea.Height,
+                    Orientation = _Orientation,
+                    Rects = Rects
+                };
+
+                _SplitWindow.EscapePressed += (s, e) =>
+                {
+                    EventHandler handler = SplitWindowHide;
+                    handler?.Invoke(this, EventArgs.Empty);
+                };
+            }
+
+            _SplitWindow.Show();
+        }
+
+
+        public void HideSplitters()
+        {
+            _SplitWindow?.Hide();
+            Rects = _SplitWindow.Rects;
+        }
+
+
+        public XElement Serialize()
+        {
+            XElement xElement = new XElement("Screen",
+                new XAttribute("Index", Index));
+
+            foreach (Rect rect in Rects)
+                xElement.Add(new XElement("rect", rect));
+
+            return xElement;
+        }
+
+
+        #endregion Public Methods
+
+    }
+}
--- WindowsManager/ViewModels/MainViewModel.cs
+++ WindowsManager/ViewModels/MainViewModel.cs
@@ -1,12 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using System.Windows;
+using System.Xml.Linq;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Ioc;
+using WindowsManager.Helpers;
+using Forms = System.Windows.Forms;
 
 namespace WindowsManager.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+
+        #region Fields
+
+        private SettingsManager _SettingsManager;
+        private bool _SettingsExist; 
+
+        #endregion Fields
+
+
+        #region Properties
+
+        public List<Screen> Screens { get; private set; } = new List<Screen>(); 
+
+        #endregion Properties
+
+
+        #region Commands
+
+        private RelayCommand _ShowSplitWindowsCommand;
+        private RelayCommand _ExitAppCommand;
+
+        public RelayCommand ShowSplitWindowsCommand => _ShowSplitWindowsCommand ??= new RelayCommand
+        (
+            () => Screens.ForEach(x => x.ShowSplitters())
+        );
+
+        public RelayCommand ExitAppCommand => _ExitAppCommand ??= new RelayCommand(() => Application.Current.Shutdown());
+
+        #endregion Commands
+
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            CheckSettings();
+            GetScreens();
+        }
+
+        #endregion Constructor
+
+
+        #region Initialisation
+
+        private void CheckSettings()
+        {
+            if (_SettingsManager is null)
+                _SettingsManager = SimpleIoc.Default.GetInstance<SettingsManager>();
+
+            _SettingsExist = _SettingsManager.SettingsExist();
+        }
+
+
+        private void LoadSettings()
+        {
+            if (_SettingsManager.Load() is IEnumerable<XElement> screenXElements)
+            {
+                foreach (XElement screenXElement in screenXElements)
+                {
+                    int index = int.Parse(screenXElement.Attribute("Index").Value);
+
+                    List<XElement> rectXElements = screenXElement.Elements().ToList();
+                    for (int i = 0; i < rectXElements.Count; i++)
+                    {
+                        List<double> rectValues = rectXElements[i].Value.Split(',').Select(x => double.Parse(x)).ToList();
+                        Screens[index].Rects[i] = new Rect(rectValues[0], rectValues[1], rectValues[2], rectValues[3]);
+                    }
+                }
+            }
+        }
+
+
+        private void GetScreens()
+        {
+            Forms.Screen[] allScreens = Forms.Screen.AllScreens;
+            for (int i = 0; i < allScreens.Length; i++)
+            {
+                Screen screen = new Screen(allScreens[i], i, _SettingsExist);
+                screen.SplitWindowHide += HideSplitters;
+                Screens.Add(screen);
+            }
+
+            if (_SettingsExist)
+                LoadSettings();
+        }
+
+        #endregion Initialisation
+
+
+        #region Event Handlers
+
+        private void HideSplitters(object sender, EventArgs e)
+        {
+            List<XElement> serializedScreens = new List<XElement>();
+            foreach (Screen screen in Screens)
+            {
+                screen.HideSplitters();
+                serializedScreens.Add(screen.Serialize());
+            }
+
+            if (_SettingsManager is null)
+                _SettingsManager = SimpleIoc.Default.GetInstance<SettingsManager>();
+
+            _SettingsManager.Save(serializedScreens);
+        } 
+
+        #endregion Event Handlers
+
     }
 }
+
+
+//TODO: change screen (Win + NumPad1 / NumPad2 / etc for each screens )
+//TODO: change rect (Win + PageUp / PageDown)
+//TODO: move (Win + Left / Up / Right / Down)
+//TODO: resize (Win + Ctrl + Left / Up / Right / Down)
+//TODO: symmetrique resize (Win + Alt + Left / Up / Right / Down)
+//TODO: fill rect (Win + Enter)
+//TODO: fill neighboor rect (Win + Shift + Left / Up / Right / Down)
