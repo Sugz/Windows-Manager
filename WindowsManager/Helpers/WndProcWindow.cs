2c759ff8378e17809aa4ee4aa88a83c15089bc5c
Clément Plantec <plantec.clement@gmail.com>
2020-08-24 03:21:10 +0200

    Create WndProcWindow.cs

---
+++ WindowsManager/Helpers/WndProcWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsManager.Helpers
+{
+    public sealed class WndProcWindow : NativeWindow
+    {
+        public event EventHandler<Message> WndProcCalled;
+
+
+        public WndProcWindow()
+        {
+            CreateHandle(new CreateParams());
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            EventHandler<Message> handler = WndProcCalled;
+            handler?.Invoke(this, m);
+
+            base.WndProc(ref m);
+        }
+
+    }
+}
