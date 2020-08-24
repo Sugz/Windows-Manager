f2d8ae97177af22eb7a7bd587c221c38eb1c0b31
Clément Plantec <plantec.clement@gmail.com>
2020-08-24 23:17:54 +0200

    Create Size.cs

---
+++ WindowsManager/Helpers/Size.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsManager.Helpers
+{
+    public class Size : ViewModelBase
+    {
+        private int _Width;
+        private int _Height;
+
+
+        public int Width 
+        {
+            get => _Width;
+            set => Set(ref _Width, value);
+        }
+
+        public int Height
+        {
+            get => _Height;
+            set => Set(ref _Height, value);
+        }
+
+
+        public bool IsNull { get; set; }
+
+        public Size() { }
+
+        public Size(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            IsNull = false;
+        }
+
+        public static Size Null => new Size { Width = 0, Height = 0, IsNull = true };
+    }
+}
